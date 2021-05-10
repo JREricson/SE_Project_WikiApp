27cabea534772dd80452df2794a3ddc0ce0c9a00
John Ericson <j.r.ericson@gmail.com>
2021-05-10 01:31:56 -0700

    few teaks to API styles, added drop down menu for userLists, created methods to find user lists (findNamesOfUserLists in databseMethods) user list data sent to viewMap page

--- helpers/databaseMethods.js
+++ helpers/databaseMethods.js
@@ -12,10 +12,7 @@ moduleObj.findUserbyId = async (userId) => {
   try {
     contentOwner = await User.findById(userId);
     console.log("found?: " + contentOwner);
-  } catch {
-    contentOwner = "undefined";
-    console.log("did not find");
-  }
+  } catch {}
 
   return contentOwner;
 };
@@ -30,8 +27,46 @@ moduleObj.findListbyId = async (listId) => {
   return theList;
 };
 
-moduleObj.findNamesOfUerLists = (userId) => {
-  let listOfNameObjs; // name: Id pair objs
+moduleObj.findListbyId = async (listId) => {
+  let contentOwner;
+  try {
+    theList = await List.findById(listId);
+  } catch {
+    theList = "undefined";
+  }
+  return theList;
+};
+
+moduleObj.findNamesOfUserLists = async (listOfListIds) => {
+  console.log("in find names of user lists");
+  let listOfNameObjs = []; // name: Id pair objs
+  // let user = await User.findById(userId);
+  try {
+    if (listOfListIds.length > 0) {
+      console.log(listOfListIds);
+      await Promise.all(
+        listOfListIds.map(async (listId) => {
+          console.log("printing lists");
+          let listObj = await List.findById(listId);
+          let listName = listObj.lst_name;
+          // console.log("list obj ", listObj);
+          console.log("list name ", listName);
+          let newObj = {};
+          newObj[listName] = listId;
+          console.log(JSON.stringify(newObj));
+
+          listOfNameObjs.push(newObj);
+        })
+      );
+      return listOfNameObjs;
+    } else {
+      console.log("no items in list");
+      return listOfNameObjs;
+    }
+  } catch {
+    console.log("something went wrong");
+    return [];
+  }
 };
 
 module.exports = moduleObj;
--- routes/mapLists.js
+++ routes/mapLists.js
@@ -10,25 +10,28 @@ const List = require("../models/list");
 
 router.get("/:userId/:listId", async (req, res) => {
   //need userItems
+
   let contentOwner = await dbMethods.findUserbyId(req.params.userId);
 
   let listObj;
   if (contentOwner) {
     listObj = await dbMethods.findListbyId(req.params.listId);
   }
+  let userLists;
   if (contentOwner) {
-    userLists = await dbMethods.findNamesOfUerLists(req.params.listId);
+    userLists = await dbMethods.findNamesOfUserLists(contentOwner.usr_listIds);
+    console.log("user lists are", userLists);
   }
 
   //TODO -- fix this ugly line
   if (typeof contentOwner != "object" || typeof listObj != "object") {
     res.status(404).render("pages/404");
   } else {
-    console.log("--liat -- \n" + listObj, contentOwner);
-    res.render("pages/viewMap", { contentOwner, listObj });
+    // console.log("--liat -- \n" + listObj, contentOwner);
+    res.render("pages/viewMap", { contentOwner, listObj, userLists });
   }
 
-  console.log("content owner -->" + contentOwner);
+  //console.log("content owner -->" + contentOwner);
   //NeedListItems
   //redirect if no user or list
 
