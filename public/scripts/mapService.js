810f243a7d7f032afe5633633db7629de5db3290
John Ericson <j.r.ericson@gmail.com>
2021-05-24 08:09:54 -0700

    Map service is now working in rough form on edit page

---
+++ public/scripts/mapService.js
@@ -0,0 +1,7 @@
+let mapScript = document.getElementById("mapScript");
+let mapScriptData = document.getElementById("map-script-string");
+
+mapScript.innerHTML = mapScriptData.dataset.script;
+
+console.log(" sc", mapScriptData.dataset.script);
+console.log("d", mapScriptData.dataset);
--- routes/mapLists.js
+++ routes/mapLists.js
@@ -31,6 +31,7 @@ router.get("/:userId/:listId", async (req, res) => {
       imgUrlList,
       textList,
       GPSList,
+      mapScript,
     });
   }
 
@@ -79,11 +80,13 @@ async function getListDetailsFromServices(req) {
     userLists = await dbMethods.findNamesOfUserLists(contentOwner.usr_listIds);
     console.log("user lists are", userLists);
   }
-  // make sure will not crahs f no url
+  // make sure will not crash if no url
   //use fetch to get image
   imgUrlList = {};
   textList = {};
   GPSList = {};
+  mapScript = "";
+
   if (listObj && listObj.lst_items) {
     //sorting through each list item
     await Promise.all(
@@ -117,7 +120,7 @@ async function getListDetailsFromServices(req) {
         let textJson = await textServiceRes.json();
 
         if (textJson["Intro"]) {
-          console.log(JSON.stringify(textJson.Intro));
+          // console.log(JSON.stringify(textJson.Intro));
           textList[ndx] = JSON.stringify(textJson.Intro);
         }
 
@@ -129,11 +132,62 @@ async function getListDetailsFromServices(req) {
         let GPSJson = await textGPSServiceRes.json();
 
         if (GPSJson) {
-          console.log(JSON.stringify(GPSJson));
+          //console.log(JSON.stringify(GPSJson));
           GPSList[ndx] = JSON.stringify(GPSJson);
         }
       })
-    );
+    ).then(async () => {
+      //mapping service
+
+      if (Object.keys(GPSList).length > 0) {
+        console.log("attempting to get map script");
+        let mapServiceJsonPost = {
+          key: process.env.MAP_KEY,
+          id: "mapid",
+        };
+
+        await Promise.all(
+          listObj.lst_items.map(async (item, ndx) => {
+            console.log("at ndx ", item.itm_Name);
+            let gpsParsed = JSON.parse(GPSList[ndx]); //GPSList[ndx]
+            let lat = gpsParsed["lat"];
+            let lon = gpsParsed["lon"];
+
+            mapServiceJsonPost[ndx] = {
+              title: `t${ndx}`, //item.itm_Name
+              coordinates: {
+                lat: Number(lat),
+                lon: Number(lon),
+              },
+            };
+          })
+        );
+        console.log("mapPost is ", mapServiceJsonPost);
+        //TODO exctract func below
+        const mapServiceRes = await fetch(
+          "https://easy-map-maker.herokuapp.com",
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(mapServiceJsonPost),
+          }
+        );
+        const mapServiceJson = await mapServiceRes.json();
+        {
+        }
+        console.log(mapServiceJson);
+
+        if (mapServiceJson && mapServiceJson.errors === "No errors.") {
+          console.log("no errors getting map script");
+          mapScript = mapServiceJson.script;
+        }
+      } else {
+        console.log("ficks this");
+      }
+    });
   }
   return { contentOwner, listObj, userLists };
 }
