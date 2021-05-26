9c5ae6624f207fea8e643995f9f46de22de3196a
John Ericson <j.r.ericson@gmail.com>
2021-05-26 13:30:44 -0700

    deleted some undeed consol logs in script.

--- public/scripts/mapService.js
+++ public/scripts/mapService.js
@@ -2,6 +2,3 @@ let mapScript = document.getElementById("mapScript");
 let mapScriptData = document.getElementById("map-script-string");
 
 mapScript.innerHTML = mapScriptData.dataset.script;
-
-console.log(" sc", mapScriptData.dataset.script);
-console.log("d", mapScriptData.dataset);
